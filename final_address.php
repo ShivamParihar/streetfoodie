cde116459324d5e490ce0528d2961466cfa9eb64
ShivamParihar <xavieranand23@gmail.com>
2019-05-04 12:53:04 +0530

    Uploading file

---
+++ final_address.php
@@ -0,0 +1,97 @@
+<?php
+session_start();
+if (!isset($_SESSION['all["phone"]'])) {
+    header("Location:index.php");
+}
+?>
+<!DOCTYPE html>
+<html lang=en>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width , initital-scale=1">       
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abel">
+        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Acme'>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="main.css">        
+        <link rel="icon" href="images/logo.png" type="image/png" sizes="16x16">
+        <title>Order one step away</title>
+        <style>
+            @media screen and (min-width: 479px){
+                .hide{display:none!important}                 
+            }
+            @media screen and (max-width: 479px){
+                .hide-low{display:none!important}                 
+            }   
+        </style>
+    </head>
+    <body>       
+        <!--header-->
+        <div class="header">
+            <div>Street foodie<small>.tk</small></div>
+        </div>
+
+        <!--navigation bar-->
+        <div class="topnav">
+            <a href="home.php" style="display:none;" class="hide-low">Home</a>
+            <a href="cart.php" class="hide-low">Cart</a>
+            <a href="your_orders.php" class="hide-low">Your orders</a>
+            <a href="profile.php" class="hide-low">Profile</a>
+            <a href="wallet.php" class="hide-low">Wallet</a>
+            <a href="offer.php" class="hide-low">Offers</a>
+            <a href="logout.php" style="float:right" class="hide-low">Logout</a>
+            <a href="javascript:void(0)" style="text-align: right;background-color:#292929;color:#fff" class="hide" onclick="myFunction()">&#9776;</a>
+        </div>
+
+        <!--navigation bar-->
+        <div id="demo4" class="topnav hide hide-low" >
+            <a href="home.php">Home</a>
+            <a href="cart.php">Cart</a>
+            <a href="your_orders.php">Your orders</a>
+            <a href="change_address.php">Change address</a>
+            <a href="wallet.php">Wallet</a>
+            <a href="offer.php">Offers</a>
+            <a href="logout.php" style="float:right" >Logout</a>
+        </div>
+        <script>
+            function myFunction() {
+                var x = document.getElementById("demo4");
+                if (x.className.indexOf("hide-low") == -1) {
+                    x.className += " hide-low";
+                } else {
+                    x.className = x.className.replace(" hide-low", " ");
+                }
+            }
+        </script>
+        
+        <!--Content-->
+        <div class="row">
+            <div class="content">
+                <?php
+                $address= $_SESSION['all["address"]'];               
+                echo '<h2>This order will be delivered to '.$address.'</h2>';                
+                ?>
+                <div class="flex-container" style="display:block;">
+                
+                <h4>To change address click on below button</h4>
+                 <p class='add_button'><a href='change_address.php'>Change</a></p><br/><br/>
+                
+                <h4>To order on this address click below</h4>
+                <p class='add_button'><a href='final_order.php'>Order</a>
+                </div>
+            </div>
+        </div>
+
+        <!--Footer-->
+        <div class="footer">
+            <div class="back_to_top"><a href=#>BACK TO TOP</a></div>
+            </br>
+            <a href="home.php">Home</a></br>	
+            <a href="cart.php">Cart</a><br/>
+            <a href="your_orders.php">Your orders</a><br/>
+            <a href="privacyPolicy.php">Privacy policy</a><br/>
+            <a href="partner.php">Partner with us</a><br/>
+            <a href="cancellation.php">Cancellation policy</a><br/>
+            &copy; 2018<?php //echo date("Y"); ?><br/>
+        </div>
+    </body>
+</html>
