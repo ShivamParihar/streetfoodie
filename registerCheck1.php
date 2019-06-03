1cceb8131792c6950eadb5ca3d6d0e225f8a718d
ShivamParihar <xavieranand23@gmail.com>
2019-06-03 21:49:49 +0530

    Uploading file

---
+++ registerCheck1.php
@@ -0,0 +1,18 @@
+<?php
+header('refresh: 5; url=index.php'); // redirect the user after 10 seconds
+#exit; // note that exit is not required, HTML can be displayed.
+?>
+
+<html>
+    <head>
+        <title>Registration Successful</title>
+        
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abel">
+    </head>
+    
+    <body style="margin:20%;color:#fff;background-color:#0073e6;text-align:center;font-family:Abel">
+        <h1>Resgistration Successful</h1>
+        <h3>You will be redirected to the login page in 5 sec</h3>
+    </body>
+</html>
+    
